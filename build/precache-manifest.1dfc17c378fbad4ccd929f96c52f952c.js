838cdb974107443f0e91fafe908a096061a3d21a
Taylor Phelps <tphelps1993@gmail.com>
2020-12-18 09:20:36 -0500

    updated readme

---
+++ build/precache-manifest.1dfc17c378fbad4ccd929f96c52f952c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3dfe0679f86ccf047ca3250efc1612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9c901aa9ce1cd1f17f9",
+    "url": "/static/css/main.cf43f0d6.chunk.css"
+  },
+  {
+    "revision": "7a3072e6462d7c8c6dbb",
+    "url": "/static/js/2.b4fd9ce5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b4fd9ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9c901aa9ce1cd1f17f9",
+    "url": "/static/js/main.e758c5d5.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
